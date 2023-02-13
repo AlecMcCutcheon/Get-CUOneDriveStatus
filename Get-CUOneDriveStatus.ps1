d3e33050af3d83b9a734a57908e93207a5958379
MixtapeXD <46252377+AlecMcCutcheon@users.noreply.github.com>
2023-02-13 12:39:01 -0500

    Update Get-CUOneDriveStatus.ps1

--- Get-CUOneDriveStatus.ps1
+++ Get-CUOneDriveStatus.ps1
@@ -17,7 +17,11 @@ Function Get-CUOneDriveStatus {
 
     [Parameter(ParameterSetName='OnDemandOnly', Mandatory=$false)]
     [Parameter(ParameterSetName='ByPathOrCLSID', Mandatory=$false)]
-    [switch]$IncludeLog
+    [switch]$IncludeLog,
+
+    [Parameter(ParameterSetName='OnDemandOnly', Mandatory=$false)]
+    [Parameter(ParameterSetName='ByPathOrCLSID', Mandatory=$false)]
+    [switch]$ForceFallback
   )
 
   $Command = "Get-ODStatus";
@@ -30,8 +34,10 @@ Function Get-CUOneDriveStatus {
 
   if (((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
     iex ((New-Object System.Net.WebClient).DownloadString("https://rawcdn.githack.com/AlecMcCutcheon/PSRunAsCurrentUser/b419b135641597982a2a4fa38e27502cde172584/PSRunAsCurrentUser.ps1"));
-    $OneDriveSync = [scriptblock]::Create('Import-Module ([System.Reflection.Assembly]::Load((Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/rodneyviana/ODSyncService/releases/latest/download/OneDriveLib.dll").content)) -ErrorAction SilentlyContinue > $null;' + " $Command|ConvertTo-Json;")
-    $Result = (PSRunAsCurrentUser -ScriptBlock $OneDriveSync | ConvertFrom-Json).Value
+    $OneDriveSync = [scriptblock]::Create('Import-Module ([System.Reflection.Assembly]::Load((Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/rodneyviana/ODSyncService/releases/latest/download/OneDriveLib.dll").content)) -ErrorAction SilentlyContinue > $null;' + " $Command|ConvertTo-Json;");
+    $PSRunAsCurrentUser = 'PSRunAsCurrentUser -ScriptBlock $OneDriveSync';
+    if ($ForceFallback) {$PSRunAsCurrentUser += " -ForceFallback";};
+    $Result = (Invoke-Expression "$PSRunAsCurrentUser" | ConvertFrom-Json).Value
   }else{
     Import-Module ([System.Reflection.Assembly]::Load((Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/rodneyviana/ODSyncService/releases/latest/download/OneDriveLib.dll").content)) -ErrorAction SilentlyContinue > $null;
     $Result = Invoke-Expression "$Command";
